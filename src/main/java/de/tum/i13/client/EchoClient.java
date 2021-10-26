135df1ef114aea60cd183f02b8449e8fd91cf01b
daviddias99 <up201705373@fe.up.pt>
2021-10-26 23:15:33 +0200

    Added disconnect method and cleanup procedures when socket/stream creation fails

--- src/main/java/de/tum/i13/client/EchoClient.java
+++ src/main/java/de/tum/i13/client/EchoClient.java
@@ -2,6 +2,7 @@ package de.tum.i13.client;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Arrays;
@@ -12,6 +13,8 @@ import de.tum.i13.shared.Constants;
 public class EchoClient {
 
   private Socket connection;
+  private InputStream inStream;
+  private OutputStream outStream;
   
   public EchoClient() {
 
@@ -23,16 +26,32 @@ public class EchoClient {
 
   public void connect(String address, int port) throws ClientException {
     try {
+
+      // Open socket and get streams
       Socket socket = new Socket(address, port);
       this.connection = socket;
+      this.inStream= this.connection.getInputStream();
+      this.outStream = this.connection.getOutputStream();
 
       // Clear existing bytes from input
       this.receive();
 
     } catch (UnknownHostException e) {
+      this.disconnect();
       throw new ClientException("Could not find host");
     } catch (IOException e) {
+      this.disconnect();
       throw new ClientException("Could not open socket");
+    }
+  }
+
+  public void disconnect() throws ClientException {
+    try {
+      this.inStream.close();
+      this.outStream.close();
+      this.connection.close();
+    } catch (IOException e) {
+      throw new ClientException("Error while closing client");
     }
   }
 
@@ -47,9 +66,8 @@ public class EchoClient {
     }
 
     try {
-      OutputStream outStream = this.connection.getOutputStream();
-      outStream.write(message);
-      outStream.flush();
+      this.outStream.write(message);
+      this.outStream.flush();
     } catch (IOException e) {
       throw new ClientException("Could not send message");
     }
@@ -60,7 +78,7 @@ public class EchoClient {
     byte[] incomingMessageBuffer = new byte[Constants.MAX_MESSAGE_SIZE_BYTES];
 
     try {
-      int numberOfReceivedBytes = this.connection.getInputStream().read(incomingMessageBuffer, 0, Constants.MAX_MESSAGE_SIZE_BYTES); 
+      int numberOfReceivedBytes = this.inStream.read(incomingMessageBuffer, 0, Constants.MAX_MESSAGE_SIZE_BYTES); 
 
       // Return an array with the size of the number of read bytes
       return Arrays.copyOf(incomingMessageBuffer, numberOfReceivedBytes);
