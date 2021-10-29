d7999aa91b3f25de185c62f7c04e41a41a020ad7
daviddias99 <up201705373@fe.up.pt>
2021-10-29 23:28:37 +0200

    Removed buggy code from tests. (left test code there by mistake)

--- src/test/java/de/tum/i13/client/TestEchoClient.java
+++ src/test/java/de/tum/i13/client/TestEchoClient.java
@@ -1,22 +1,14 @@
 package de.tum.i13.client;
 
 import org.junit.jupiter.api.*;
-import org.mockito.internal.util.reflection.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-
-
 
 import java.net.Socket;
 
 public class TestEchoClient {
-
-  private Socket mySocket;
   
   @BeforeEach //gets called before the class initializes, perfect place to connect to a server
   public void initialize() {
-    mySocket = mock(Socket.class);
   }
 
   @AfterEach //tearing down, perfect place to properly disconnect from a server or close a socket
@@ -26,8 +18,6 @@ public class TestEchoClient {
 
   @Test //each method with Test annotation gets called
   public void testFramework() {
-      EchoClient client = new EchoClient();
-      FieldSetter setter;
       assertEquals(true, true, "We can't trust Java");
   }
 }
