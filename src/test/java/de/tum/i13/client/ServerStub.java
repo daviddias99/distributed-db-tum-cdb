acfa27a7db60cc6ed1d204baddf5e314d1efd52d
Lukas Bernwald <lukas.bernwald@tum.de>
2021-11-02 20:42:55 +0100

    Revert "Merge branch 'milestone_1' into 'master'"
    This reverts merge request !6

--- src/test/java/de/tum/i13/client/ServerStub.java
+++ src/test/java/de/tum/i13/client/ServerStub.java
@@ -7,58 +7,58 @@ import java.net.UnknownHostException;
 
 public class ServerStub implements Runnable {
 
-    private ServerSocket localServer;
-    public int connectionsMade = 0;
+  private ServerSocket localServer;
+  public int connectionsMade = 0;
 
-    public static final int BYTES_PER_KB = 1024;
-    public static final int MAX_MESSAGE_SIZE_KB = 128;
-    public static final int MAX_MESSAGE_SIZE_BYTES = MAX_MESSAGE_SIZE_KB * BYTES_PER_KB;
+  public static final int BYTES_PER_KB = 1024;
+  public static final int MAX_MESSAGE_SIZE_KB = 128;
+  public static final int MAX_MESSAGE_SIZE_BYTES = MAX_MESSAGE_SIZE_KB * BYTES_PER_KB;
 
-    public ServerStub(ServerSocket serverSocket) {
-        this.localServer = serverSocket;
+  public ServerStub(ServerSocket serverSocket) {
+    this.localServer = serverSocket;
+  }
+
+  public synchronized Socket accept() {
+    Socket clientSocket = null;
+    try {
+      connectionsMade++;
+      clientSocket = this.localServer.accept();
+    } catch (IOException e) {
+      // e.printStackTrace();
     }
+    return clientSocket;
+  }
 
-    public synchronized Socket accept() {
-        Socket clientSocket = null;
-        try {
-            connectionsMade++;
-            clientSocket = this.localServer.accept();
-        } catch (IOException e) {
-            // e.printStackTrace();
+  @Override
+  public void run() {
+
+    byte[] serverGreet = "Welcome!".getBytes();
+    byte[] serverAnswer = "Answer!".getBytes();
+
+    byte[] buffer = new byte[MAX_MESSAGE_SIZE_BYTES];
+
+    while (true) {
+      try {
+        Socket clientSocket = this.accept();
+
+        if(clientSocket == null) {
+          continue;
         }
-        return clientSocket;
-    }
 
-    @Override
-    public void run() {
-
-        byte[] serverGreet = "Welcome!".getBytes();
-        byte[] serverAnswer = "Answer!".getBytes();
-
-        byte[] buffer = new byte[MAX_MESSAGE_SIZE_BYTES];
+        clientSocket.getOutputStream().write(serverGreet);
 
         while (true) {
-            try {
-                Socket clientSocket = this.accept();
-
-                if(clientSocket == null) {
-                    continue;
-                }
-
-                clientSocket.getOutputStream().write(serverGreet);
-
-                while (true) {
-                    int readResult = clientSocket.getInputStream().read(buffer);
-                    if (readResult == -1) {
-                        break;
-                    }
-                    clientSocket.getOutputStream().write(serverAnswer);
-                }
-            } catch (UnknownHostException e) {
-                // e.printStackTrace();
-            } catch (IOException e) {
-                // e.printStackTrace();
-            }
+          int readResult = clientSocket.getInputStream().read(buffer);
+          if (readResult == -1) {
+            break;
+          }
+          clientSocket.getOutputStream().write(serverAnswer);
         }
+      } catch (UnknownHostException e) {
+        // e.printStackTrace();
+      } catch (IOException e) {
+        // e.printStackTrace();
+      }
     }
+  }
 }
