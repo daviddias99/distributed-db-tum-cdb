de518d4f632d90d478a1f941de64d81ce1cc9da2
Lukas Bernwald <lukas.bernwald@tum.de>
2021-11-05 16:31:14 +0100

    Add basic kv message implementation

--- src/main/java/de/tum/i13/server/kv/KVMessage.java
+++ src/main/java/de/tum/i13/server/kv/KVMessage.java
@@ -2,35 +2,45 @@ package de.tum.i13.server.kv;
 
 public interface KVMessage {
 
-    public enum StatusType {
-        GET, 			/* Get - request */
-        GET_ERROR, 		/* requested tuple (i.e. value) not found */
-        GET_SUCCESS, 	/* requested tuple (i.e. value) found */
-        PUT, 			/* Put - request */
-        PUT_SUCCESS, 	/* Put - request successful, tuple inserted */
-        PUT_UPDATE, 	/* Put - request successful, i.e. value updated */
-        PUT_ERROR, 		/* Put - request not successful */
-        DELETE, 		/* Delete - request */
-        DELETE_SUCCESS, /* Delete - request successful */
-        DELETE_ERROR 	/* Delete - request successful */
+    enum StatusType {
+        GET(true), 			/* Get - request */
+        GET_ERROR(true), 		/* requested tuple (i.e. value) not found */
+        GET_SUCCESS(false), 	/* requested tuple (i.e. value) found */
+        PUT(true), 			/* Put - request */
+        PUT_SUCCESS(false), 	/* Put - request successful, tuple inserted */
+        PUT_UPDATE(false), 	/* Put - request successful, i.e. value updated */
+        PUT_ERROR(false), 		/* Put - request not successful */
+        DELETE(true), 		/* Delete - request */
+        DELETE_SUCCESS(true), /* Delete - request successful */
+        DELETE_ERROR(true); 	/* Delete - request successful */
+
+        private final boolean emptyValue;
+
+        StatusType(boolean emptyValue) {
+            this.emptyValue = emptyValue;
+        }
+
+        public boolean canHaveEmptyValue() {
+            return this.emptyValue;
+        }
     }
 
     /**
      * @return the key that is associated with this message,
      * null if not key is associated.
      */
-    public String getKey();
+    String getKey();
 
     /**
      * @return the value that is associated with this message,
      * null if not value is associated.
      */
-    public String getValue();
+    String getValue();
 
     /**
      * @return a status string that is used to identify request types,
      * response types and error types associated to the message.
      */
-    public StatusType getStatus();
+    StatusType getStatus();
 
 }
---
+++ src/main/java/de/tum/i13/server/kv/KVMessageImpl.java
@@ -0,0 +1,43 @@
+package de.tum.i13.server.kv;
+
+import de.tum.i13.shared.Preconditions;
+
+public class KVMessageImpl implements KVMessage {
+
+    private final String key;
+    private final String value;
+    private final StatusType status;
+
+    public KVMessageImpl(String key, StatusType status) {
+        Preconditions.notNull(key);
+        Preconditions.check(status.canHaveEmptyValue());
+        this.key = key;
+        this.value = null;
+        this.status = status;
+    }
+
+    public KVMessageImpl(String key, String value, StatusType status) {
+        Preconditions.notNull(key);
+        Preconditions.notNull(value);
+        this.key = key;
+        this.value = value;
+        this.status = status;
+    }
+
+    @Override
+
+    public String getKey() {
+        return this.key;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public StatusType getStatus() {
+        return this.status;
+    }
+
+}
