92cca1c217f8f8a83013363c8f026ec5365598a6
daviddias99 <up201705373@fe.up.pt>
2021-11-20 10:33:54 +0100

    Add documentation for Pair

--- src/main/java/de/tum/i13/server/persistentStorage/btree/chunk/Pair.java
+++ src/main/java/de/tum/i13/server/persistentStorage/btree/chunk/Pair.java
@@ -2,6 +2,10 @@ package de.tum.i13.server.persistentStorage.btree.chunk;
 
 import java.io.Serializable;
 
+/**
+ * Data class representing a {@link Serializable} key-value pair. Keys are Strings and value types are configured through the generic {@code V}
+ * @param <V> Type of vlaue
+ */
 public class Pair<V> implements Serializable {
 
   private static final long serialVersionUID = 6529685098267755681L;
@@ -9,6 +13,11 @@ public class Pair<V> implements Serializable {
   public final String key;
   public final V value;
 
+  /**
+   * Create new pair
+   * @param key key
+   * @param value value
+   */
   public Pair(String key, V value) {
     this.key = key;
     this.value = value;
