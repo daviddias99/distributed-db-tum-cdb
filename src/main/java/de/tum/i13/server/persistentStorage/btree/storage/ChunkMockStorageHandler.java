901ac5daf92a9abcef1b59634e775f2d24a3877a
daviddias99 <up201705373@fe.up.pt>
2021-11-20 11:44:24 +0100

    Add logging to mock chunk storage handler

--- src/main/java/de/tum/i13/server/persistentStorage/btree/storage/ChunkMockStorageHandler.java
+++ src/main/java/de/tum/i13/server/persistentStorage/btree/storage/ChunkMockStorageHandler.java
@@ -2,31 +2,38 @@ package de.tum.i13.server.persistentStorage.btree.storage;
 
 import java.io.Serializable;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import de.tum.i13.server.persistentStorage.btree.chunk.Chunk;
 
 /**
  * This class provides an in-memory implementation of
- * {@link ChunkStorageHandler}. Is is mainly use for testing in order
- * to abstract from disk storage issues.
+ * {@link ChunkStorageHandler}. Is is mainly use for testing in order to
+ * abstract from disk storage issues.
  */
-public class ChunkMockStorageHandler<V> implements ChunkStorageHandler<V>, Serializable{
+public class ChunkMockStorageHandler<V> implements ChunkStorageHandler<V>, Serializable {
+  private static final Logger LOGGER = LogManager.getLogger(PersistentBTreeMockStorageHandler.class);
 
   private static final long serialVersionUID = 6529685098267757691L;
 
   private Chunk<V> chunk;
 
   @Override
-  public Chunk<V> readChunk() {    
+  public Chunk<V> readChunk() {
+    LOGGER.trace("Loaded chunk ({}) from memory.", this.hashCode());
     return this.chunk.clone();
   }
 
   @Override
   public void storeChunk(Chunk<V> chunk) {
+    LOGGER.trace("Stored chunk ({}) in memory.", this.hashCode());
     this.chunk = chunk;
   }
 
   @Override
   public void storeChunkForce(Chunk<V> chunk) {
-    this.chunk = chunk;    
+    LOGGER.trace("Stored chunk ({}) in memory.", this.hashCode());
+    this.chunk = chunk;
   }
 }
