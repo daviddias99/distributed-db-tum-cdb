fd4217012b3a800e641d3f59815e70a1026bf56a
daviddias99 <up201705373@fe.up.pt>
2021-11-20 10:45:14 +0100

    Fixed storage exception docs

--- src/main/java/de/tum/i13/server/persistentStorage/btree/storage/StorageException.java
+++ src/main/java/de/tum/i13/server/persistentStorage/btree/storage/StorageException.java
@@ -7,7 +7,7 @@ package de.tum.i13.server.persistentStorage.btree.storage;
 public class StorageException extends Exception {
 
   /**
-   * Creates a new {@link KVException} with the supplied message
+   * Creates a new {@link StorageException} with the supplied message
    *
    * @param message the message of the exception
    */
@@ -16,7 +16,7 @@ public class StorageException extends Exception {
   }
 
   /**
-   * Creates a new {@link KVException} with the supplied message and cause
+   * Creates a new {@link StorageException} with the supplied message and cause
    *
    * @param message the message of the exception
    * @param cause the cause of the exception
@@ -26,7 +26,7 @@ public class StorageException extends Exception {
   }
 
   /**
-   * Creates a new {@link KVException} with the supplied format string and objects
+   * Creates a new {@link StorageException} with the supplied format string and objects
    *
    * @param format the string to format
    * @param args the parameters for the string formatting
@@ -37,7 +37,7 @@ public class StorageException extends Exception {
   }
 
   /**
-   * Creates a new {@link KVException} with the supplied format string and objects, and a cause
+   * Creates a new {@link StorageException} with the supplied format string and objects, and a cause
    *
    * @param format the string to format
    * @param args the parameters for the string formatting
