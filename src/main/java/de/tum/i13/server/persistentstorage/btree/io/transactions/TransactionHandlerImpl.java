1e8bbfef6126c6d9a04585701e976c95746384d8
daviddias99 <up201705373@fe.up.pt>
2021-11-26 17:10:28 +0100

    Refactored common code in TransactionHandlerImpl

--- src/main/java/de/tum/i13/server/persistentstorage/btree/io/transactions/TransactionHandlerImpl.java
+++ src/main/java/de/tum/i13/server/persistentstorage/btree/io/transactions/TransactionHandlerImpl.java
@@ -54,6 +54,18 @@ public class TransactionHandlerImpl<V> implements TransactionHandler<V> {
     this(storageFolder, DEFAULT_DIRECTORY);
   }
 
+  private void chunkTransfer(Path src, Path dst) throws StorageException {
+    try {
+      StorageUtils.copyAndReplaceFile(src, dst);
+
+    } catch (IOException e) {
+      StorageException ex = new StorageException(e, "An error occured during chunk transfer");
+      LOGGER.error(Constants.THROWING_EXCEPTION_LOG_MESSAGE, ex);
+      this.endTransaction();
+      throw ex;
+    }
+  }
+
   @Override
   public void notifyChunkChange(String chunkId) throws StorageException {
     if (!transactionStarted) {
@@ -63,18 +75,10 @@ public class TransactionHandlerImpl<V> implements TransactionHandler<V> {
     // Make a copy if it's an existant (before the start of transaction) chunk that
     // is changing.
     if (!createdChunks.contains(chunkId) && changedChunks.add(chunkId)) {
-      Path dst = Paths.get(this.storageFolder, this.backupFolder, chunkId);
+
       Path src = Paths.get(this.storageFolder, chunkId);
-
-      try {
-        StorageUtils.copyAndReplaceFile(src, dst);
-
-      } catch (IOException e) {
-        StorageException ex = new StorageException(e, "An error occured during rollback");
-        LOGGER.error(Constants.THROWING_EXCEPTION_LOG_MESSAGE, ex);
-        this.endTransaction();
-        throw ex;
-      }
+      Path dst = Paths.get(this.storageFolder, this.backupFolder, chunkId);
+      this.chunkTransfer(src, dst);
     }
   }
 
@@ -99,14 +103,7 @@ public class TransactionHandlerImpl<V> implements TransactionHandler<V> {
     for (String chunkId : changedChunks) {
       Path src = Paths.get(this.storageFolder, this.backupFolder, chunkId);
       Path dst = Paths.get(this.storageFolder, chunkId);
-      try {
-        StorageUtils.copyAndReplaceFile(src, dst);
-      } catch (IOException e) {
-        StorageException ex = new StorageException(e, "An error occured during rollback");
-        LOGGER.error(Constants.THROWING_EXCEPTION_LOG_MESSAGE, ex);
-        this.endTransaction();
-        throw ex;
-      }
+      this.chunkTransfer(src, dst);
     }
 
     // Delete newly created chunks
@@ -123,19 +120,9 @@ public class TransactionHandlerImpl<V> implements TransactionHandler<V> {
     }
 
     // Replace tree structure
-    try {
-      StorageUtils.copyAndReplaceFile(Paths.get(this.storageFolder, this.backupFolder, "root"),
-          Paths.get(this.storageFolder, "root"));
-    } catch (IOException e) {
-      StorageException ex = new StorageException(e, "An error occured during rollback");
-      LOGGER.error(Constants.THROWING_EXCEPTION_LOG_MESSAGE, ex);
-      this.endTransaction();
-      throw ex;
-    }
-
+    this.chunkTransfer(Paths.get(this.storageFolder, this.backupFolder, "root"), Paths.get(this.storageFolder, "root"));
     this.endTransaction();
     @SuppressWarnings("unchecked")
-
     // Read new root and return it
     PersistentBTreeNode<V> newRoot = (PersistentBTreeNode<V>) StorageUtils
         .readObject(Paths.get(this.storageFolder, "root"));
@@ -153,16 +140,8 @@ public class TransactionHandlerImpl<V> implements TransactionHandler<V> {
 
     // Create backup directory
     StorageUtils.createDirectory(Paths.get(this.storageFolder, this.backupFolder));
-    try {
-      // Copy tree root
-      StorageUtils.copyAndReplaceFile(Paths.get(this.storageFolder, "root"),
-          Paths.get(this.storageFolder, this.backupFolder, "root"));
-    } catch (IOException e) {
-      StorageException ex = new StorageException(e, "An error occured during begin transaction");
-      LOGGER.error(Constants.THROWING_EXCEPTION_LOG_MESSAGE, ex);
-      this.endTransaction();
-      throw ex;
-    }
+    // Copy tree root
+    this.chunkTransfer(Paths.get(this.storageFolder, "root"), Paths.get(this.storageFolder, this.backupFolder, "root"));
   }
 
   public void endTransaction() {
