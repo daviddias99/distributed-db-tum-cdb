179e8b5b2deed1449d38997809c929ae4e45d26a
Lukas Bernwald <lukas.bernwald@tum.de>
2021-11-29 23:37:11 +0100

    Add documentation to network location data class

--- src/main/java/de/tum/i13/shared/NetworkLocationImpl.java
+++ src/main/java/de/tum/i13/shared/NetworkLocationImpl.java
@@ -1,11 +1,25 @@
 package de.tum.i13.shared;
 
+/**
+ * Basic data class implementation of {@link NetworkLocation}
+ */
 public class NetworkLocationImpl implements NetworkLocation {
 
     private final String address;
     private final int port;
 
+    /**
+     * Creates a new {@link NetworkLocation} with the given address and port
+     *
+     * @param address the host address of the {@link NetworkLocation}, must not be null
+     * @param port    the port of the {@link NetworkLocation}, must be between 0 and 65535 inclusive
+     */
     public NetworkLocationImpl(String address, int port) {
+        Preconditions.notNull(address, "Host address must not be null");
+        Preconditions.check(
+                port >= 0 && port <= 65535,
+                "Port number must be between 0 and 65535 inclusive");
+
         this.address = address;
         this.port = port;
     }
