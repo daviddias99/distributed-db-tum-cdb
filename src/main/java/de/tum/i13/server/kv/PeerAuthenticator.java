f4ce9683dfca866e92012e8a871ffd6c54f1dbcf
Lukas Bernwald <lukas.bernwald@tum.de>
2022-01-27 14:38:33 +0100

    Refactor peer authenticator and add more message

--- src/main/java/de/tum/i13/server/kv/PeerAuthenticator.java
+++ src/main/java/de/tum/i13/server/kv/PeerAuthenticator.java
@@ -6,140 +6,86 @@ package de.tum.i13.server.kv;
  */
 public class PeerAuthenticator {
 
-  /**
-   * Type of a peer
-   */
-  public enum PeerType {
     /**
-     * client
+     * Type of a peer
      */
-    CLIENT(true, false),
+    public enum PeerType {
+        /**
+         * client
+         */
+        CLIENT(true, false),
 
-    /**
-     * server
-     */
-    SERVER(false, true),
+        /**
+         * server
+         */
+        SERVER(false, true),
 
-    /**
-     * ECS
-     */
-    ECS(false, true),
+        /**
+         * ECS
+         */
+        ECS(false, true),
 
-    /**
-     * Any of the previous values
-     */
-    ANY(false, true);
+        /**
+         * Any of the previous values
+         */
+        ANY(false, true);
 
-    private final boolean needsGreet;
-    private final boolean bypasseStop;
+        private final boolean needsGreet;
+        private final boolean bypassesStop;
 
-    /**
-     * A peer type is associated with values that indicate if the peer needs to
-     * receive a greet upon connection and if it can still perform actions while a
-     * server is in a stopped state.
-     * 
-     * @param needsGreet  true if peer needs greet
-     * @param bypasseStop true if peer can bypass server STOPPED state
-     */
-    private PeerType(boolean needsGreet, boolean bypasseStop) {
-      this.needsGreet = needsGreet;
-      this.bypasseStop = bypasseStop;
+        /**
+         * A peer type is associated with values that indicate if the peer needs to
+         * receive a greet upon connection and if it can still perform actions while a
+         * server is in a stopped state.
+         *
+         * @param needsGreet   true if peer needs greet
+         * @param bypassesStop true if peer can bypass server STOPPED state
+         */
+        private PeerType(boolean needsGreet, boolean bypassesStop) {
+            this.needsGreet = needsGreet;
+            this.bypassesStop = bypassesStop;
+        }
+
+        /**
+         * Check if peer type needs server greet
+         *
+         * @return true if peer needs server greet
+         */
+        public boolean needsGreet() {
+            return needsGreet;
+        }
+
+        /**
+         * Check if peer type can bypass server stopped state
+         *
+         * @return true if peer can bypass server stopped state
+         */
+        public boolean canBypassStop() {
+            return this.bypassesStop;
+        }
     }
 
     /**
-     * Check if peer type needs server greet
-     * 
-     * @return true if peer needs server greet
+     * Get peer type from message status type
+     *
+     * @param type message status type
+     * @return peer type associated with message status type
      */
-    public boolean needsGreet() {
-      return needsGreet;
+    public static PeerType authenticate(KVMessage.StatusType type) {
+        return switch (type) {
+            case GET, PUT, KEYRANGE, DELETE -> PeerType.CLIENT;
+            case SERVER_HANDOFF_ACK, SERVER_WRITE_UNLOCK, GET_ERROR, GET_SUCCESS, PUT_SERVER, PUT_ERROR, PUT_SUCCESS,
+                    PUT_UPDATE, DELETE_SUCCESS, DELETE_ERROR, SERVER_STOPPED, SERVER_HANDOFF_SUCCESS,
+                    SERVER_NOT_RESPONSIBLE, SERVER_START, SERVER_ACK, SERVER_WRITE_LOCK, SERVER_SHUTDOWN,
+                    SERVER_HEART_BEAT, KEYRANGE_SUCCESS, CHORD_CLOSEST_PRECEDING_FINGER, CHORD_FIND_SUCCESSOR,
+                    CHORD_GET_PREDECESSOR, CHORD_NOTIFY, CHORD_GET_STATE_STR, CHORD_HEARTBEAT,
+                    CHORD_HEARTBEAT_RESPONSE, CHORD_FIND_SUCESSSOR_RESPONSE, CHORD_CLOSEST_PRECEDING_FINGER_RESPONSE,
+                    CHORD_GET_PREDECESSOR_RESPONSE, CHORD_GET_SUCCESSORS, CHORD_GET_SUCCESSOR_RESPONSE,
+                    CHORD_NOTIFY_ACK -> PeerType.SERVER;
+            case ECS_WRITE_LOCK, ECS_WRITE_UNLOCK, ECS_HANDOFF, ECS_SET_KEYRANGE, ECS_HEART_BEAT, ECS_ACK,
+                    ECS_WAITING_FOR_HANDOFF -> PeerType.ECS;
+            case ERROR, CHORD_GET_STATE_STR_RESPONSE -> PeerType.ANY;
+        };
     }
 
-    /**
-     * Check if peer type can bypass server stopped state
-     * 
-     * @return true if peer can bypass server stopped state
-     */
-    public boolean canBypassStop() {
-      return this.bypasseStop;
-    }
-  }
-
-  /**
-   * Get peer type from message status type
-   * 
-   * @param type message status type
-   * @return peer type associated with message status type
-   */
-  public static PeerType authenticate(KVMessage.StatusType type) {
-    // TODO: PeerAuthenticator isn't checking all types
-    switch (type) {
-      case ERROR:
-        return PeerType.ANY;
-      case GET:
-        return PeerType.CLIENT;
-      case GET_ERROR:
-        return PeerType.SERVER;
-      case GET_SUCCESS:
-        return PeerType.SERVER;
-      case PUT:
-        return PeerType.CLIENT;
-      case PUT_SERVER:
-        return PeerType.SERVER;
-      case PUT_ERROR:
-        return PeerType.SERVER;
-      case PUT_SUCCESS:
-        return PeerType.SERVER;
-      case PUT_UPDATE:
-        return PeerType.SERVER;
-      case DELETE:
-        return PeerType.CLIENT;
-      case DELETE_SUCCESS:
-        return PeerType.SERVER;
-      case DELETE_ERROR:
-        return PeerType.SERVER;
-      case SERVER_STOPPED:
-        return PeerType.SERVER;
-      case SERVER_HANDOFF_SUCCESS:
-        return PeerType.SERVER;
-      case SERVER_NOT_RESPONSIBLE:
-        return PeerType.SERVER;
-      case SERVER_START:
-        return PeerType.SERVER;
-      case SERVER_ACK:
-        return PeerType.SERVER;
-      case SERVER_WRITE_LOCK:
-        return PeerType.SERVER;
-      case SERVER_SHUTDOWN:
-        return PeerType.SERVER;
-      case SERVER_HEART_BEAT:
-        return PeerType.SERVER;
-      case ECS_WRITE_LOCK:
-        return PeerType.ECS;
-      case ECS_WRITE_UNLOCK:
-        return PeerType.ECS;
-      case ECS_HANDOFF:
-        return PeerType.ECS;
-      case ECS_SET_KEYRANGE:
-        return PeerType.ECS;
-      case ECS_HEART_BEAT:
-        return PeerType.ECS;
-      case KEYRANGE:
-        return PeerType.CLIENT;
-      case KEYRANGE_SUCCESS:
-        return PeerType.SERVER;
-      case CHORD_CLOSEST_PRECEDING_FINGER:
-        return PeerType.SERVER;
-      case CHORD_FIND_SUCCESSOR:
-        return PeerType.SERVER;
-      case CHORD_GET_PREDECESSOR:
-        return PeerType.SERVER;
-      case CHORD_NOTIFY:
-        return PeerType.SERVER;
-      case CHORD_GET_STATE_STR:
-        return PeerType.SERVER;
-      default:
-        return PeerType.ANY;
-    }
-  }
 }
